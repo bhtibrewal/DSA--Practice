ae7c3b21cbf8b9b5b1f18aadf1d137e612de30df
Bhavika Tibrewal <bhtibrewal@gmail.com>
2022-09-23 00:16:54 +0530

    Time: 126 ms (50.25%), Space: 46.1 MB (8.07%) - LeetHub

---
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let k = 0, curr =0 ;
+    for (let i in nums){
+        // console.log(nums)
+        if(nums[curr] !== nums[i]) {
+            curr++;
+            nums[curr] = nums[i]
+        }
+    }
+    return curr+1
+};
