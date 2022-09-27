35ba9971ce0467b1ce19bf550f3b9e281da2a3dd
Bhavika Tibrewal <bhtibrewal@gmail.com>
2022-09-27 12:04:55 +0530

    Time: 271 ms (17.59%), Space: 53.2 MB (63.34%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.js
+++ 49-group-anagrams/49-group-anagrams.js
@@ -5,7 +5,13 @@
 var groupAnagrams = function(strs) {
     let map = {}
     for (let word of strs){
-        let key = [...word].sort().join();
+        
+         let key = [...word].sort().join();// time complex-> klogk
+
+        // let count = new Array(26).fill(0);
+        // for (let char of word) count[char.charCodeAt()-97]++;
+        // let key = count.join("#");  // time complex-> k
+
          map[key]? map[key].push(word) : map[key] = [word]
         
     }
