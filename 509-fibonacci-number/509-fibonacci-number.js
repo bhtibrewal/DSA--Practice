1e65a323b25b26a48eb629a574b8745c185b75c5
Bhavika Tibrewal <bhtibrewal@gmail.com>
2022-09-28 20:33:16 +0530

    Time: 98 ms (55.94%), Space: 42 MB (33.00%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.js
+++ 509-fibonacci-number/509-fibonacci-number.js
@@ -3,18 +3,15 @@
  * @return {number}
  */
 var fib = function(n) {
- 
-   const dp = Array(n+1).fill(-1);
-   
-    
-    return helper(n, dp )
+
+    if(n == 0) return 0;
+   const dp = [0, 1];
+    let i=2;
+    while(i++<=n){
+        let temp= dp[0]+dp[1]
+        dp[0] = dp[1];
+        dp[1]=temp
+    }
+    return dp[1]
+
 };
-
-const helper = (n, dp)=>{
-    if (n<=1) return n
-    if (dp[n] !== -1) return dp[n]
-    
-    dp[n] = helper(n-1, dp) + helper(n-2, dp)
-
-    return dp[n]
-}
