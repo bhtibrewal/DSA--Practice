59cb50f3b53b028539abeb30e9fe162495e1d445
Bhavika Tibrewal <bhtibrewal@gmail.com>
2022-09-28 21:47:23 +0530

    Time: 84 ms (76.95%), Space: 42.6 MB (77.28%) - LeetHub

---
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minCostClimbingStairs = function(cost) {
+    let n = cost.length, i = 2
+    
+        if(n <= 2) return Math.min(cost[0] , cost[1])
+        
+        while (i< n){
+            let temp = Math.min(cost[0] , cost[1]) + cost[i++];
+
+            cost[0] = cost[1];
+            cost[1] = temp
+        }
+    
+    return Math.min(cost[0] , cost[1])
+};
+
