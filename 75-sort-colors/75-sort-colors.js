02a4f6389c4bc170d997ffa0e6c39a14b07126f3
Bhavika Tibrewal <bhtibrewal@gmail.com>
2022-10-01 01:16:26 +0530

    Time: 127 ms (6.03%), Space: 42.1 MB (50.58%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+    let low=0, mid=0, high=nums.length-1;
+    for (let i in nums){
+        if (nums[mid]==0){
+            swap(nums, low, mid);
+            low++;
+            mid++;
+        }
+        else if(nums[mid]==1){
+            mid++;
+        }
+        else {
+            swap(nums, mid, high);
+            high--;
+        }
+    }
+};
+function swap(nums, a, b){
+    let c=nums[a];
+    nums[a] = nums[b];
+    nums[b] = c;
+}
