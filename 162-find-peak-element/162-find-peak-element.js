384700a1188202b3e4ad559d84ae2d6953e81447
Bhavika Tibrewal <bhtibrewal@gmail.com>
2022-10-02 19:45:27 +0530

    Time: 60 ms (96.18%), Space: 41.9 MB (77.24%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.js
+++ 162-find-peak-element/162-find-peak-element.js
@@ -2,19 +2,35 @@
  * @param {number[]} nums
  * @return {number}
  */
+
+// O(n) -> time complexity 
+// find peak by linearly interating over the arr
+/**
+const findPeakElement = (nums)=>{
+    for(let i=0; i<nums.length; i++){
+        console.log(nums[i])
+        if(nums[i] > nums[i+1]) return i
+    }
+    return nums.length - 1
+}
+**/
+
+// O(log n) -> time complexity 
+// iterative binary search
 var findPeakElement = function(nums) {
-    let start = 0, end = nums.length - 1; let mid = Math.floor((end + start)/ 2);
+    let start = 0, end = nums.length - 1; 
+    let mid = Math.floor((end + start)/ 2);
+    
     while (start < end){
 
-        mid = Math.floor((end + start)/ 2);
-        
-        
+        // condition for peak value
         if (nums[mid] > nums[mid+1] && nums[mid] > nums[mid-1]) break;
         
-        if (nums[mid] > nums[mid+1]) end = mid;
-        else start = mid +1 ;
+        if (nums[mid] > nums[mid+1]) end = mid; // right side of peak
+        else start = mid +1 ;// left side of peak
+        
         mid = Math.floor((end + start)/ 2);
-        console.log(mid, start, end)
     }
     return mid
 };
+
