9877e7e1ecb81883da278269861273dfe2f3b9e4
Bhavika Tibrewal <bhtibrewal@gmail.com>
2022-10-03 18:53:17 +0530

    format: find-first-and-last-position-element

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.js
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.js
@@ -3,41 +3,29 @@
  * @param {number} target
  * @return {number[]}
  */
-var searchRange = function(nums, target) {
+var searchRange = function (nums, target) {
     let start = 0, end = nums.length - 1; let res = [-1, -1]
-    
-    
-    while(start < end){
-        let mid = Math.floor((start + end) / 2);
-        if(nums[mid]>= target){
-            end = mid
-        }
-        else {
-            start = mid + 1;
-        }
 
+
+    while (start < end) {
+        let mid = Math.floor((start + end) / 2);
+
+        nums[mid] >= target ? end = mid : start = mid + 1;
     }
-    console.log("l", start, end)
-    if(nums[start]!==target) return res;
-    res[0] = start
-    
-    //
-     end = nums.length - 1;
 
-    while(start < end){
+    if (nums[start] !== target) return res;
+    res[0] = start
+
+    //
+    end = nums.length - 1;
+
+    while (start < end) {
         let mid = Math.floor((start + end) / 2);
 
-        if(nums[mid]<= target ){
-            start = mid + 1;
-        }
-        else {
-            end = mid;
-        }
-
+        nums[mid] <= target ? start = mid + 1 : end = mid;
     }
 
     res[1] = nums[start] == target ? start : start - 1
-    
-    console.log("-----------------")
+
     return res
 };
