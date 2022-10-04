793eb2b9db0c850e4dc2ad13af636841c2cca97e
Bhavika Tibrewal <bhtibrewal@gmail.com>
2022-10-05 02:54:26 +0530

    add notes

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.js
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.js
@@ -7,9 +7,12 @@ var findKthLargest = function(nums, k) {
     const pq = new MinPriorityQueue();
     let n = nums.length;
     
+    // form a min heap of k elements 
     for (let i=0; i< k; i++)
         pq.enqueue(nums[i]);
     
+    // for rest of the elements keep on checking if 
+    // currEl is greater than the topEl dequeue the topEl and add the currEl
     for(let i=k; i<n; i++ ){
         let topEl = pq.front().element;
         if (topEl < nums[i]){
@@ -17,6 +20,8 @@ var findKthLargest = function(nums, k) {
             pq.enqueue(nums[i]);
         }
     }
+    // finally we have a min heap of k largest elements in the array
+    // so the topEl is the kth largest el
     
     console.log(pq);
     return pq.front().element;
