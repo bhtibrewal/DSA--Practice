30e9c6e10bdd4f952d8807188fb2adcb2a68b207
Bhavika Tibrewal <bhtibrewal@gmail.com>
2022-10-08 20:47:35 +0530

    Time: 104 ms (32.22%), Space: 43.9 MB (6.95%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.js
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.js
@@ -4,13 +4,26 @@
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
-    let col = matrix[0].length;
-    for (let row = 0; row<= matrix.length-1; row++){
-        if (matrix[row][0] <= target && matrix[row][col- 1] >= target){
-            let ans = binarySearch(matrix[row], target, 0, col-1);
+    let col = matrix[0].length - 1, row= matrix.length-1;
+    
+    let start = 0, end = row;
+    let mid = Math.floor((start + end)/2);
+    
+    while (start <= end){
+        console.log(start, mid, end)
+        if (matrix[mid][0] <= target && matrix[mid][col] >= target){
+            let ans = binarySearch(matrix[mid], target, 0, col);
 
             return ans!==-1 ? true : false
         }
+        if(matrix[mid][0] > target){
+            end = mid - 1;    
+        }
+        else {
+            start = mid + 1;
+        }
+        
+        mid = Math.floor((start + end)/2);
     }
     return false;
 };
