65726bf9c193b8e60abc0558325ba8e8751d1cd8
Bhavika Tibrewal <bhtibrewal@gmail.com>
2022-12-27 00:48:38 +0530

    Added solution - LeetHub

---
+++ BFS of graph - GFG/bfs-of-graph.js
@@ -0,0 +1,89 @@
+//{ Driver Code Starts
+// Initial Template for javascript
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => { inputString += inputStdin; });
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(
+        string => { return string.trim(); });
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    let t = parseInt(readLine());
+    for (let i = 0; i < t; i++) {
+        let input_line = readLine().split(' ');
+        let V = parseInt(input_line[0]);
+        let E = parseInt(input_line[1]);
+
+        let adj = new Array(V);
+        for (let i = 0; i < V; i++) {
+            adj[i] = new Array();
+        }
+        for (let i = 0; i < E; i++) {
+            input_line = readLine().split(' ');
+            let x = input_line[0];
+            let y = input_line[1];
+            adj[x].push(y);
+        }
+
+        let obj = new Solution();
+        let ans = obj.bfsOfGraph(V, adj);
+        let s = "";
+        for (let i = 0; i < ans.length; i++) {
+            s += ans[i];
+            s += " ";
+        }
+        console.log(s);
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for javascript
+
+/**
+ * @param {number} V
+ * @param {number[][]} adj
+ * @returns {number[]}
+*/
+class Solution {
+    // Function to return Breadth First Traversal of given graph.
+    bfsOfGraph(V, adj) {
+	const bfs_transversal = [],
+		visited = Array(V).fill(false);
+
+	visited[0] = true;
+	let queue = [0];
+
+	while (queue.length) {
+		let s = Number(queue[0]);
+		queue.shift();
+		bfs_transversal.push(s);
+		let length = adj[s].length;
+
+		while (length--) {
+			let adjacent = adj[s].shift();
+			
+			if (!visited[adjacent]) {
+				visited[adjacent] = true;
+				queue.push(adjacent);
+			}
+		}
+	}
+	
+	return bfs_transversal;
+}
+}
