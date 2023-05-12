0f2f8daf36219a86339924605e88f9de71c43758
Bhavika Tibrewal <bhtibrewal@gmail.com>
2023-05-12 19:22:59 +0530

    Update Promise.js

--- 🌟 GreatFrontendMentor/Promise.js
+++ 🌟 GreatFrontendMentor/Promise.js
@@ -2,15 +2,16 @@ export default function promiseAll(iterable) {
 	const result = [];
 	return new Promise(async (resolve, reject) => {
 		if (!iterable.length) resolve(result);
-
+		let resolved = 0;
 		for (let i in iterable) {
 			try {
 				const res = await iterable[i];
-				result.push(res);
+				result[] = res;
+				resolved++;
 			} catch (err) {
 				reject(err);
 			} finally {
-				if (iterable.length === result.length) resolve(result);
+				if (resolved === result.length) resolve(result);
 			}
 		}
 	});
